27a2ecbfc053ebe61a45e07e4542a07d5575bc35
Raphael Dray <dray.raph@gmail.com>
2020-05-09 04:34:36 +0200

    Added UserModel, some utils to simplify and refactored Controller

---
+++ src/main/java/com/corochat/app/utils/setters/LinkSetter.java
@@ -0,0 +1,19 @@
+package com.corochat.app.utils.setters;
+
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Paint;
+
+public class LinkSetter {
+    private static final String COLOR_LINK = "#0000EE";
+    private static final String COLOR_BASIS = "#948e8e";
+
+    public static void setAsLink(final Label label) {
+        label.setCursor(Cursor.HAND);
+        label.setTextFill(Paint.valueOf(COLOR_LINK));
+    }
+
+    public static void unsetAsLink(final Label label) {
+        label.setTextFill(Paint.valueOf(COLOR_BASIS));
+    }
+}
