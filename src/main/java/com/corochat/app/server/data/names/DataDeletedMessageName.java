665c8639a82dfb231a4dc1af322e5a94557571f9
yulypso <khamphousone.thierry@outlook.fr>
2020-05-28 03:57:13 +0200

    added message and deleted_message tables

--- src/main/java/com/corochat/app/server/data/names/DataDeletedMessageName.java
+++ src/main/java/com/corochat/app/server/data/names/DataDeletedMessageName.java
@@ -1,6 +1,6 @@
 package com.corochat.app.server.data.names;
 
-public final class DataDeletedMessage {
+public final class DataDeletedMessageName {
     public static final String TABLE_NAME = "corochat_deleted_message";
     public static final String COL_ID = "id";
     public static final String COL_MESSAGE_ID = "message_id";
--- src/main/java/com/corochat/app/server/data/names/DataMessageName.java
+++ src/main/java/com/corochat/app/server/data/names/DataMessageName.java
@@ -5,5 +5,5 @@ public final class DataMessageName {
     public static final String COL_ID = "id";
     public static final String COL_USER_EMAIL = "user_email";
     public static final String COL_MESSAGE = "message";
-    public static final String COL_DATE = "date";
+    public static final String COL_DATE = "date_time";
 }
