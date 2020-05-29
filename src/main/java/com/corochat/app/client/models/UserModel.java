5fc73d4700162e255d1b21ea46c219e064aa17f3
Raphael Dray <dray.raph@gmail.com>
2020-05-29 18:03:43 +0200

    Added javaDoc for UserModel

--- src/main/java/com/corochat/app/client/models/UserModel.java
+++ src/main/java/com/corochat/app/client/models/UserModel.java
@@ -2,6 +2,24 @@ package com.corochat.app.client.models;
 
 import java.util.Objects;
 
+/**
+ * <h1>The UserModel object</h1>
+ * <p>
+ *     An UserModel is an object that has:
+ *     <ul>
+ *         <li>A first name</li>
+ *         <li>A last name</li>
+ *         <li>A pseudo</li>
+ *         <li>An email</li>
+ *         <li>An hashed password</li>
+ *     </ul>
+ * </p>
+ * //TODO Include diagram of UserModel
+ *
+ * @author Raphael Dray
+ * @version 0.0.3
+ * @since 0.0.1
+ */
 public class UserModel {
     private String firstName;
     private String lastName;
@@ -10,6 +28,14 @@ public class UserModel {
     private String hashedPassword;
     private boolean active;
 
+    /**
+     * This constructor initialize these attributes
+     * @param firstName The first name of the user
+     * @param lastName The last name of the user
+     * @param pseudo The pseudo of the user
+     * @param email The email of the user
+     * @param hashedPassword The hashed password of the user
+     */
     public UserModel(final String firstName,
                      final String lastName,
                      final String pseudo,
@@ -35,6 +61,11 @@ public class UserModel {
                 '}';
     }
 
+    /**
+     * Compute the equality of two users
+     * @param o The object to be compared to
+     * @return boolean - The answer to the question of the objects are equal
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -47,23 +78,43 @@ public class UserModel {
                getHashedPassword().equals(userModel.getHashedPassword());
     }
 
+    /**
+     * Compute the hashcode to perform an UID
+     * @return int - The hashcode of the user
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getFirstName(), getLastName(), getPseudo(), getEmail(), getHashedPassword());
     }
 
+    /**
+     * Getter of the first name
+     * @return String - The first name of the client
+     */
     public String getFirstName() {
-        return firstName;
+        return this.firstName;
     }
 
+    /**
+     * Setter of the first name
+     * @param firstName The first name of the client
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Getter of the last name
+     * @return String - The last name of the client
+     */
     public String getLastName() {
-        return lastName;
+        return this.lastName;
     }
 
+    /**
+     * Setter of the last name
+     * @param lastName 
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
